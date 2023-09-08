bafea802f1ebb214ab1fcb0b1f95bdac3b4523b4
TOMILOBA <oluwatomilobaomojola@gmail.com>
2023-09-07 18:23:54 -0700

    link to github file index.jas added

--- utils/result.js
+++ utils/result.js
@@ -7,7 +7,7 @@ const current_day=date.toLocaleDateString(undefined,{weekday:`long`})
      "current_day": current_day,
      "utc_time": date.toISOString(),
      "track": "backend",
-     "github_file_url": "Tomiloba2",
+     "github_file_url": "https://github.com/Tomiloba2/Hng-stage1/blob/master/index.js",
      "github_repo_url": "https://github.com/Tomiloba2/Hng-stage1.git",
      "status_code": 200
  }]
